26200416d59ea16e98951caed204a806d039fb82
CrazyBird <roy7alexandre@gmail.com>
2020-12-03 21:13:38 +0100

    Bit Dive

--- main.c
+++ main.c
@@ -6,110 +6,41 @@
 //#define MAXCHAR 8
 
 
-/*Have to implement sub and div first…*/
-char _MOD(struct num *A, struct num *B) {
-	return 0;
-}
-/*
-[IDEA] :
-recursive DIV(N,D,S) //S= 0 for the first call
+
+void __DIV(struct num *N,struct num* D)
 {
-	Dl = D; (copy);
-	Q = struct num 1;
-
-	while(N>(Dl + S)) :
-		Dl<<1;
-		Q<<1;
-	
-	//1 step back
-	Dl >>1;
-	Q >>1;
-
-	IF (N-Dl)>D :
-		Q,Dl += recursive DIV(N,D,Dl);
-	
-	R = N-Dl;
-	return Q,Dl,R;
-}
-*/
-/*First without R*/
-/*InerDIV
-Start at prevD wich is the last D*2 before D become greater than N
-*/
-struct num* _inerDIV(struct num* N, struct num* D, struct num* prevD)
-{
-	struct num *Dl = newNum(1 + N->Size);
-	copyNum(D,Dl);
-	struct num * Q = newNum(N->Size); //Optimiser la taille de Q ?
-	Q->Num[0]=0x01;
-	struct num* R = newNum(1+ N->Size); //R register for iner calculus.
-
-	//Fast forward 
-	_ADD(Dl,prevD,R);
-	while(_CMP(N,R)>=0)
+	struct num * Q = newNum(N->Size);
+	struct num * R = newNum(N->Size);
+	struct num * calcul = newNum(N->Size);
+	signed short i = (N->Size * 8) -1;
+	char mask = 0x01;
+	char index = 0;
+	while(i >= 0)
 	{
-		_LSHIFT(Dl,1);
-		_LSHIFT(Q,1);
-		clearNum(R);
-		_ADD(Dl,prevD,R);
-		printf(">");
+		_LSHIFT(R,1);
+		R->Num[0] |= (N->Num[i] & mask);
+
+		if(_CMP(R,D)>=0)
+		{
+			clearNum(calcul);
+			_SUB(R,D,calcul);
+			copyNum(calcul,R);
+			Q->Num[i] |= mask ;
+		}
+
+		mask = mask << 1;
+		if(mask == 0)
+		{
+			mask = 1;
+			index ++;
+		}
+		i--;
 	}
-	//One step back
-	_RSHIFT(Dl,1);
-	_RSHIFT(Q,1); //One step back for Q
-	_M1(Q); //Q less one cause we start à 1 and no 0
-
-	clearNum(R);
-	_ADD(prevD,Dl,R);
-	printf("inerdiv\n");
-	printNum(Dl);
-	printNum(prevD);
-
-	copyNum(R,prevD);
-	delNum(Dl);
-	delNum(R);
-	return Q;
-}
-
-struct num* _laDIV(struct num* N, struct num* D)
-{
-	struct num *Dl = newNum(1 + D->Size);
-	copyNum(D,Dl);
-	struct num * Q = newNum(D->Size); //Optimiser la taille de Q ?
-	Q->Num[0]=0x01;
-
-	struct num * R = newNum(D->Size); 
-
-	//Fast forward 
-	while(_CMP(N,Dl)>0)
-	{
-		_LSHIFT(Dl,1);
-		_LSHIFT(Q,1);
-	}
-	//One step back
-	_RSHIFT(Dl,1);
-	_RSHIFT(Q,1); //One step back for Q
-	_M1(Q); //Q less one cause we start à 1 and no 0
-
-	
-	while(_CMP(N,Dl)>0)
-	{
-		struct num* Qr;
-		clearNum(R);
-		Qr = _inerDIV(N,D,Dl);
-		_ADD(Qr,Q,R);
-		copyNum(R,Q);
-		delNum(Qr);
-		printf("outerdiv\n");
-		printNum(Q);
-		printNum(Dl);
-	}
-	delNum(R);
-	delNum(Dl);
-	printf("\n");
 	printNum(Q);
-	/*Real result is Q-1*/
-	return Q;
+	printNum(R);
+	delNum(calcul);
+	delNum(R);
+	delNum(Q);
 }
 
 
@@ -133,7 +64,7 @@ int main(void) {
 	printNum(A);
 	printNum(B);
 
-	_laDIV(B,A);
+	__DIV(B,A);
 
 	return 0;
 }
